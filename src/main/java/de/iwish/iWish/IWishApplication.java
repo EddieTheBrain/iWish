e8c286348650129f6a1f10fa79146c42c34dcd98
Eddie <eddie.lamprecht@icloud.com>
2024-10-18 11:09:45 +0200

    Initial Commit

---
+++ src/main/java/de/iwish/iWish/IWishApplication.java
@@ -0,0 +1,13 @@
+package de.iwish.iWish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IWishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IWishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/iwish/iWish/IWishApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iwish.iWish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IWishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
