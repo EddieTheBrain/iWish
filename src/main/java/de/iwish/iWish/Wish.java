20963f27d2a7fcf02c98f1a04c7a928b4bd4c977
Eddie <eddie.lamprecht@icloud.com>
2024-10-29 10:00:52 +0100

    implemented Id in Wish

--- src/main/java/de/iwish/iWish/Wish.java
+++ src/main/java/de/iwish/iWish/Wish.java
@@ -6,11 +6,13 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class Wish {
+    private final UUID wishId = UUID.randomUUID();
     private String name;
     private BigDecimal price;
     private LocalDate dateOfCreation;
