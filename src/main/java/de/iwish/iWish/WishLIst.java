decfa8fb8a7c89b10b2c08bf5d1206ae4ce64383
Eddie <eddie.lamprecht@icloud.com>
2024-10-29 10:00:35 +0100

    added WishList

---
+++ src/main/java/de/iwish/iWish/WishLIst.java
@@ -0,0 +1,9 @@
+package de.iwish.iWish;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class WishLIst {
+    private final UUID wishListId = UUID.randomUUID();
+    private ArrayList<Wish> wishes;
+}
