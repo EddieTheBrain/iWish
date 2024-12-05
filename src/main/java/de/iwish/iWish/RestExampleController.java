0c4a32f41e1148f152252626865d07b5c9e39f2f
Eddie <eddie.lamprecht@icloud.com>
2024-12-05 20:03:07 +0100

    Backend Render

--- src/main/java/de/iwish/iWish/RestExampleController.java
+++ src/main/java/de/iwish/iWish/RestExampleController.java
@@ -1,22 +1,13 @@
 package de.iwish.iWish;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
-@Controller
+@RestController
 public class RestExampleController {
     @GetMapping(path = "/wish")
-    public ResponseEntity<List<Wish>> getWishes() {
-        ArrayList<Wish> wishInstances = new ArrayList<>();
-        wishInstances.add(new Wish("Fußball", BigDecimal.valueOf(10.9), LocalDate.of(2024, 4, 1), "www.amazon.de", Wish.Priority.MITTEL));
-        wishInstances.add(new Wish("Laptop", BigDecimal.valueOf(999.99), LocalDate.of(2024, 12, 25), "www.ebay.de", Wish.Priority.HOCH));
-        wishInstances.add(new Wish("Buch", BigDecimal.valueOf(14.99), LocalDate.of(2024, 6, 15), "www.buch.de", Wish.Priority.NIEDRIG));
-        return ResponseEntity.ok(wishInstances);
+    public List<Wish> getAllWishes(){
+        return service.getAll;
     }
 }
