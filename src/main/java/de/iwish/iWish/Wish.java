678e35990ce639a155db2b4c646e9e7478d90f03
vincent <vincent.e@t-online.de>
2024-12-10 13:48:59 +0100

    entity wish class

--- src/main/java/de/iwish/iWish/Wish.java
+++ src/main/java/de/iwish/iWish/Wish.java
@@ -1,16 +1,20 @@
 package de.iwish.iWish;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Wish {
     private final UUID wishId = UUID.randomUUID();
     private String name;
