83a940fd570c821a31cf01e396bfd4050564af15
Eddie <eddie.lamprecht@icloud.com>
2024-12-12 18:53:12 +0100

    Lucas hat gerettet

--- src/main/java/de/iwish/iWish/Wish.java
+++ src/main/java/de/iwish/iWish/Wish.java
@@ -1,37 +1,21 @@
 package de.iwish.iWish;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-@Entity
 @Getter
 @Setter
-@NoArgsConstructor
+@AllArgsConstructor
 public class Wish {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private final UUID wishId = UUID.randomUUID();
     private String name;
     private BigDecimal price;
     private LocalDate dateOfCreation;
     private String link;
     private Priority priority; public enum Priority{HOCH, MITTEL, NIEDRIG}
-
-    public Wish(String name, BigDecimal price, LocalDate dateOfCreation, String link, Priority priority) {
-        this.name = name;
-        this.price = price;
-        this.dateOfCreation = dateOfCreation;
-        this.link = link;
-        this.priority = priority;
-    }
 }
--- src/main/java/de/iwish/iWish/WishRepository.java
+++
@@ -1,9 +0,0 @@
-package de.iwish.iWish;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WishRepository extends CrudRepository<Wish, Integer> {
-
-}
