b983363971d53d83be91ee5f4bc7f7b8e3ead939
Eddie <eddie.lamprecht@icloud.com>
2024-12-12 20:43:05 +0100

    made Wish an Entity

--- src/main/java/de/iwish/iWish/Wish.java
+++ src/main/java/de/iwish/iWish/Wish.java
@@ -1,21 +1,38 @@
 package de.iwish.iWish;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Wish {
-    private final UUID wishId = UUID.randomUUID();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private BigDecimal price;
     private LocalDate dateOfCreation;
     private String link;
     private Priority priority; public enum Priority{HOCH, MITTEL, NIEDRIG}
+
+    public Wish(String name, BigDecimal price, LocalDate dateOfCreation, String link, Priority priority) {
+        this.name = name;
+        this.price = price;
+        this.dateOfCreation = dateOfCreation;
+        this.link = link;
+        this.priority = priority;
+    }
 }
